b8e55d862a098e58caea643d19957c72f3e784f3
Pontus <pontus.u.o.eliasson@gmail.com>
2020-02-24 10:24:07 +0100

    Initial creation
    Created project, class etc

---
+++ src/SimpleSearchEngine/SimpleSearchEngine.java
@@ -0,0 +1,11 @@
+package SimpleSearchEngine;
+
+public class SimpleSearchEngine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SimpleSearchEngine");
+
+	}
+
+}
